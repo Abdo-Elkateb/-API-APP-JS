fd2be5906581e30fa986310253eb231b58f4d8dc
Abdo Elkateb <elkatebdev@gmail.com>
2023-07-20 21:16:47 +0200

    last commit

--- main.js
+++ main.js
@@ -1,23 +1,52 @@
 // js 
 
-let cardImage = document.getElementById("cardImage"),
+const cardImage = document.getElementById("cardImage"),
     user = document.getElementById("user"),
     email = document.getElementById("email"),
     phone = document.getElementById("phone"),
     address = document.getElementById("address"),
     showNewUser = document.getElementById("showNewUser"),
-    info = document.getElementById("info");
+    info = document.getElementById("info"),
+cardClick = document.querySelectorAll(".card_click button");
+const sers = "https://randomuser.me/api/";
+   let data = [];
 
-let cardIpi = new XMLHttpRequest();
-cardIpi.open("GET", "./card.json");
-cardIpi.send();
+function getUserData() {
+    info.textContent = "loading......................"
+    let cardIpi = new XMLHttpRequest();
+    cardIpi.open("GET", sers);
+    cardIpi.send();
 
-cardIpi.onload = function () {
+    cardIpi.onload = function () {
 
-    console.log(JSON.parse(cardIpi.responseText).links)
-    console.log(cardIpi.responseText)
-    console.log(JSON.stringify(cardIpi.responseText))
+        let newData = cardIpi.responseText;
+        data = JSON.parse(newData).results[0]
+        // console.log(data)
+        console.log(data)
+        cardImage.src = data.picture.large
+        email.textContent = data.email
+        user.textContent = data.name.first + " " + data.name.last
+        info.textContent = ""
+        
 
+    }
 }
+getUserData() 
+
+
+cardClick.forEach(function(item){
+item.addEventListener("click", function(){
+    if(this.dataset.info == "phone") {
+        info.textContent = data.phone;
+        info.style.color = "red"
+    }else {
+        info.textContent = " Country / " + data.location.country  + " || City / " + "" + data.location.city;
+        info.style.color = "blue"
+    }
+})
+})
+
+
+showNewUser.addEventListener("click", getUserData )
 
 
